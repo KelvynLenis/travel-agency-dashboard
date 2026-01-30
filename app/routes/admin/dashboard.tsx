b1a4f965c28a553a16ecec222ec838c0efa76d81
KelvynLenis <kmartinslenis30@hotmail.com>
2026-01-30 16:42:25 -0300

    feat: add sidebar and header

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,5 +1,21 @@
+import { Header } from "components";
+
 const dashboard = () => {
-  return <div>dashboard</div>;
+  const user = {
+    name: "John Doe",
+    email: "p0m5o@example.com",
+    imageUrl: "/assets/images/david.webp",
+  };
+
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome ${user.name ?? "Guest"}`}
+        description={"Track your trips and activities"}
+      />
+      Dashboard
+    </main>
+  );
 };
 
 export default dashboard;
